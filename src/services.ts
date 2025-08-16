6401a719f8d744205df5ce6c1feffac12bd8043f
kelvis castillo <castillokelvis560@gmail.com>
2025-08-16 18:34:07 -0500

    details

--- src/services.ts
+++ src/services.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { EXCHANGE_RATE } from "./utils/contants";
 import { RaffleType } from "./utils/types";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "kjcastillo.up.railway.app";
 
 export const submitTicket = async (values: any) => {
   try {
